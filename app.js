e92ad7e0406357cde3996204ff63656b2bcec771
Alisson Zampietro <alisson.zampietro@gmail.com>
2022-03-09 17:24:05 +0000

    feat: setting up docker-compose

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 import * as amqp from 'amqplib'
+import dotenv from 'dotenv'
 import http from 'http'
 
-const PORT = 3002
+dotenv.config()
 
 const handler = (req, res) => {
     res.setHeader('content-type', 'application/json')
     res.end(JSON.stringify({response:'its working'}))
 }
 
-http.createServer(handler).listen(PORT, () => console.log(`RUNNING AT ${PORT}`))
+http.createServer(handler).listen(process.env.PORT, () => console.log(`RUNNING AT ${process.env.PORT}`))
